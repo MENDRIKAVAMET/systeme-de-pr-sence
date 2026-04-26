d9511a375645e295797b158b676ef84d179cd648
mendrika <mendrikavamet@gmail.com>
2026-04-26 14:58:03 +0300

    Première commit pour l'envoye au groupe

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include "loginwindow.h"
-
+#include <QProcess>
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    //Démarrage automatique du serveur node.js
+    QProcess::startDetached("node", QStringList() << "server.js");
+
     LoginWindow w;
     w.show();
     return a.exec();
